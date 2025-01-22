df70709833a0fcc94d84f3fa68ad3fdf9ef9692a
Casielim <e1300513@u.nus.edu>
2025-01-23 04:07:59 +0800

    Add echo

--- src/main/java/Vera.java
+++ src/main/java/Vera.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Vera{
     private static final String line = "_________________________________________________";
 
@@ -5,9 +7,22 @@ public class Vera{
         String greetings = "Hello! I'm Vera\nWhat can I do for you?";
         String bye = "Bye. Hope to see you again soon!";
 
+        Scanner sc = new Scanner(System.in);
+
+        //greetings part
         System.out.println(line);
         System.out.println(greetings);
         System.out.println(line);
+
+        //echo part
+        String s = sc.nextLine();
+        while (!s.equals("bye")) {
+            System.out.println(s);
+            System.out.println(line);
+            s = sc.nextLine();
+        }
+
+        //exit part
         System.out.println(bye);
         System.out.println(line);
     }
