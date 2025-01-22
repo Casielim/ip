5f0ead44871e972513f65586ef3ecc1bddbe4bc4
Casielim <e1300513@u.nus.edu>
2025-01-23 03:32:34 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Vera.java
@@ -0,0 +1,15 @@
+public class Vera{
+    private static final String line = "_________________________________________________";
+
+    public static void main(String[] args) {
+        String greetings = "Hello! I'm Vera\nWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(greetings);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+    }
+}
+
