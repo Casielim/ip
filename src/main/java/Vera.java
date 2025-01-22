3d280c3bc3e36a14bffaf2e5a9ce6643d262bd57
Casielim <e1300513@u.nus.edu>
2025-01-23 04:32:25 +0800

    Add support for list

--- src/main/java/Vera.java
+++ src/main/java/Vera.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Vera{
     private static final String line = "  ______________________________________________________";
+    private static List<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String greetings = "  Hello! I'm Vera\n  What can I do for you?";
@@ -18,6 +21,7 @@ public class Vera{
         String s = sc.nextLine();
         while (!s.equals("bye")) {
             System.out.println("  " + s);
+            list.add(s);
             System.out.println(line);
             s = sc.nextLine();
         }
