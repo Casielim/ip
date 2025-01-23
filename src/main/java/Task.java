85fb8415ae4dadc26139012bd722f90356803407
Casielim <e1300513@u.nus.edu>
2025-01-23 14:15:58 +0800

    add mark, unmark, toString feature

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -10,4 +10,21 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "X" : " "); //mark done task with X
     }
+
+    public void markFeature() {
+        System.out.println("  Nice! I've marked this task as done:");
+        this.isDone = true;
+        System.out.println("   " + this.toString());
+    }
+
+    public void unmarkFeature() {
+        System.out.println("  OK, I've marked this task as not done yet:");
+        this.isDone = false;
+        System.out.println("   " + this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), this.description);
+    }
 }
